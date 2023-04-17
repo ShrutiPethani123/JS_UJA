5e93c2948e5c82c620d6bbe240a5f450776394a9
unknown <shrutipethani.royal@gmail.com>
2023-04-17 10:53:21 +0530

    first

---
+++ first.js
@@ -0,0 +1,29 @@
+console.log("Hello.....")
+// alert("Important Notice!!")
+
+
+// var , let , const
+
+var a = 34
+var b = 45
+
+console.log(a+b);
+
+const c = 34
+// c=56
+console.log(c);
+
+
+let name1="uja"
+let age=23
+
+console.log(name1 +" "+age);
+
+
+// single line 
+
+/*
+
+
+*/
+
---
+++ operators.js
@@ -0,0 +1,34 @@
+/*
+
+1. Aritmetic Operator (+ - * / %  ** ++ --)
+2. Assignment operator 
+3. Relational operator
+4. Logical operator
+5. Bitwise Operator
+6. Ternary operator
+
+*/
+
+let a = 4
+let b = 6
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(2**3);
+console.log(a++); // 4
+console.log(a); // 5
+console.log(++a); // 6
+console.log(a); // 6
+
+
+
+
+
+
+
+
+
+
